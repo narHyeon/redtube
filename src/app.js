16f66f0eb41c19b1ae184a7366856bb90d92054c
nar <yoyo7227@naver.com>
2021-01-06 23:33:01 +0900

    show video list with title

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
+import React, {useState, useEffect} from 'react';
 import './app.css';
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-    return <h1>Hello :)</h1>;
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDbRDDdAgBi5G8WKIlojRj_dOCMqXE8Mpg", requestOptions)
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }, []);
+    return <VideoList videos={videos} />;
 }
 
 export default App;
