4b221aafabe0354e5a329f72bc5460e31f9a4d30
nar <yoyo7227@naver.com>
2021-01-07 21:04:48 +0900

    back to the list when searching

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ import React, {useState, useEffect} from 'react';
 import styles from './app.module.css';
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({youtube}) {
     const [videos, setVideos] = useState([]);
-    const search = query => {
-        youtube.search(query).then(videos => setVideos(videos));
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const selectVideo = video => {
+        setSelectedVideo(video);
+    }
+
+    const search = async (query) => {
+        const response = await youtube.search(query);
+        setVideos(response);
+        setSelectedVideo(null);
     };
 
     useEffect(() => {
@@ -15,7 +24,17 @@ function App({youtube}) {
     return (
         <div className={styles.app}>
             <SearchHeader onSearch={search}/>
-            <VideoList videos={videos} />
+            <section className={styles.content}>
+                {selectedVideo && <div className={styles.detail}>
+                    <VideoDetail video={selectedVideo} />
+                </div>}
+                <div className={styles.list}>
+                    <VideoList
+                        videos={videos}
+                        onVideoClick={selectVideo}
+                        display={selectedVideo ? 'list' : 'grid'}/>
+                </div>
+            </section>
         </div>
     );
 }
